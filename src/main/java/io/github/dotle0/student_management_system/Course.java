0d11c07e76ea92e2db91d10189851cfa051dfb25
DotLe0 <petko2604yonkov@gmail.com>
2025-10-06 21:47:10 +0300

    Created Student, Course and CourseGrade classes

---
+++ src/main/java/io/github/dotle0/student_management_system/Course.java
@@ -0,0 +1,19 @@
+package io.github.dotle0.student_management_system;
+
+public class Course {
+	private String title;
+
+	public Course(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+}
---
+++ src/main/java/io/github/dotle0/student_management_system/Student.java
@@ -0,0 +1,72 @@
+package io.github.dotle0.student_management_system;
+
+import java.util.List;
+
+public class Student {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String grade;
+	private List<CourseGrade> coursesGrades;
+	
+	
+	public Student(int id, String firstName, String lastName, String grade)
+	{
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grade = grade;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public List<CourseGrade> getCoursesGrades() {
+		return coursesGrades;
+	}
+
+	public void setCoursesGrades(List<CourseGrade> coursesGrades) {
+		this.coursesGrades = coursesGrades;
+	}
+	
+	public double calculateAverageGrade()
+	{
+		double total = 0;
+		for(CourseGrade grade : this.coursesGrades) {
+			total += grade.getGrade();
+		}
+		double average = total / this.coursesGrades.size();
+		System.out.println("Average grade: " + average);
+		return average;
+	}
+	
+	public void AddGrade(String courseTitle, double grade) {
+		Course course = new Course(courseTitle);
+		CourseGrade newGrade = new CourseGrade(course, grade);
+		
+		this.coursesGrades.add(newGrade);
+		System.out.println("New grade added");
+	}
+}
