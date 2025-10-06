72529e0d4b94dfc2ed36e4c05d97cfc9157073a9
DotLe0 <petko2604yonkov@gmail.com>
2025-10-06 22:39:29 +0300

    Added SQLite

--- src/main/java/io/github/dotle0/student_management_system/App.java
+++ src/main/java/io/github/dotle0/student_management_system/App.java
@@ -1,13 +1,13 @@
 package io.github.dotle0.student_management_system;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static void main( String[] args )
     {
-        
+    	wellcomeText();
+    }
+    
+    public static void wellcomeText() {
+    	System.out.println("Wellcome to Student-Managment-System");
     }
 }
