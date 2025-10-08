65d453d92603500c0b5d951a7c08baffa33ecb1e
DotLe0 <petko2604yonkov@gmail.com>
2025-10-08 21:52:50 +0300

    Working add, remove,average methods for CourseGradesDAO

--- src/main/java/dao/CourseGradeDAO.java
+++ src/main/java/dao/CourseGradeDAO.java
@@ -1,7 +1,82 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.Course;
+import model.Student;
+
 public class CourseGradeDAO {
-	//Add grade
-	//remove grade
-	//Student average based on all grades, and per course
+	//Show students grades
+	//Show students grades per course
+	private Connection connection;
+	
+	public CourseGradeDAO(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public void addCourseGrade(Student s, Course c, float grade) throws SQLException {
+		String sql = "INSERT INTO CourseGrade (student_id, course_id, grade) VALUES ("
+				+ "    (SELECT id FROM students WHERE firstName = ? AND lastName = ?),"
+				+ "    (SELECT id FROM course WHERE title = ?),"
+				+ "    ?);";
+		
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, s.getFirstName());
+		stmt.setString(2, s.getLastName());
+		stmt.setString(3, c.getTitle());
+		stmt.setFloat(4, grade);
+		
+		stmt.executeUpdate();
+		System.out.println("Added a grade to student.");
+	}
+	
+	public void removeGrade(Student s, Course c, float grade)throws SQLException {
+		String sql = "DELETE FROM CourseGrade WHERE "
+				+ "    student_id = (SELECT id FROM students WHERE firstName = ? AND lastName = ?)"
+				+ "    AND"
+				+ "    course_id = (SELECT id FROM course WHERE title = ?)"
+				+ "    AND grade=?;";
+		
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, s.getFirstName());
+		stmt.setString(2, s.getLastName());
+		stmt.setString(3, c.getTitle());
+		stmt.setFloat(4, grade);
+		
+		stmt.executeUpdate();
+		System.out.println("Deleted a grade.");
+	}
+	
+	public void getStudentAverageGrade(Student s) throws SQLException {
+		String sql = "SELECT AVG(grade) FROM CourseGrade WHERE "
+				+ "    student_id=(SELECT id FROM students WHERE firstName=? AND lastName=?);";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, s.getFirstName());
+		stmt.setString(2, s.getLastName());
+		ResultSet rs = stmt.executeQuery();
+		
+		float avr = rs.getFloat(1);
+		
+		System.out.println("Average student grade: " + avr);
+	}
+	
+	public void getStudentAverageGradeForCourse(Student s, Course c) throws SQLException {
+		String sql ="SELECT AVG(grade) FROM CourseGrade WHERE "
+				+ "    student_id=(SELECT id FROM students WHERE firstName=? AND lastName=?)"
+				+ "    AND"
+				+ "    course_id=(SELECT id FROM course WHERE title=?);";
+		
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, s.getFirstName());
+		stmt.setString(2, s.getLastName());
+		stmt.setString(3, c.getTitle());
+		ResultSet rs = stmt.executeQuery();
+		
+		float avr = rs.getFloat(1);
+		
+		System.out.println("Average grade for course: " + avr);
+	}
 }
--- src/main/java/io/github/dotle0/student_management_system/main/App.java
+++ src/main/java/io/github/dotle0/student_management_system/main/App.java
@@ -1,7 +1,6 @@
 package io.github.dotle0.student_management_system.main;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import dao.*;
@@ -14,15 +13,27 @@ public class App
     {
     	wellcomeText();
     	
-    	
-    	
+    	CourseGradeDAO courseGradeDao = new CourseGradeDAO(DBhelper.getConnection());
+    	StudentDAO studentDao = new StudentDAO(DBhelper.getConnection());
     	CourseDAO courseDao = new CourseDAO(DBhelper.getConnection());
     	
-    	List<Course> courses = courseDao.showAllCourses();
+    	Student student1 = new Student("Liliya", "Mateeva", "12A");
+    	Course course1 = new Course("Programirane");
+    	Course course2 = new Course("BEL");
+    	/*
+    	studentDao.addStudent(student1);
+    	courseDao.addCourse(course1);
+    	courseDao.addCourse(course2);
+    	*/
+    	//courseGradeDao.addCourseGrade(student1, course1, 6);
+    	//courseGradeDao.addCourseGrade(student1, course1, 4);
+    	//courseGradeDao.addCourseGrade(student1, course1, 6);
+    	//courseGradeDao.addCourseGrade(student1, course2, 6);
 
-    	for(Course course : courses) {
-    		courseDao.removeCourse(course);
-    	}
+    	courseGradeDao.getStudentAverageGrade(student1);
+    	courseGradeDao.getStudentAverageGradeForCourse(student1, course1);
+    	
+    	courseGradeDao.removeGrade(student1, course2, 6);
     	
     	DBhelper.closeConnection();
     	
